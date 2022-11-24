7a126909ba6e6a2726da4924f7860d247c7741d0
SHAJAN29 <118879955+SHAJAN29@users.noreply.github.com>
2022-11-24 14:03:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const resume = {
+  "name": "shajan",
+  "age": "23",
+  "gender": "male",
+  "email": "m.j.sandy2913@gmail.com",
+  "study": ["bsc(viscual_communation)"],
+  "experience": ["internship in cauvery NEWS", "prasad corp ltd"],
+
+  "langauges_known": ["Tamil", "English", "telegu"],
+};
+for (let i = 0; i <resume.experience.length; i++) {
+    console.log(resume.experience[i]);
+  }
+
+
+let arr = [
+  {
+    name: "shajan",
+    age: "23",
+  },
+  {
+    name: "priya",
+    age: "20",
+  },
+  {
+    name: "moses",
+    age: "50",
+  },
+  {
+    name:"jaya",
+    age:"45"  }
+]
+//for (let i in arr) {
+  //  console.log(arr[i].name);
+ // }
+//for (let i = 0; i <arr.length; i++) {
+ // console.log(arr[i].name,":",arr[i].age);
+//}
